0fa070df20b9bfc7fac0cf904c940de6fc52ee9d
Pavel Stetina <github@shark.kom.cz>
2018-02-26 16:48:37 +0100

    Simplify interface TreeNode to describe children (single) linked nodes Add ImmutaleTreeNode which is traversable only in parents -> children direction

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -7,10 +7,6 @@ public interface TreeNode<T> {
 	 */
 	public TreeNode<T> addChild(TreeNode<T> child);
 
-	/** Returns ancestor from desired generation if exists.
-	 */
-	public TreeNode<T> getAncestor(int generations);
-
 	/** Returns the child TreeNode at index.
 	 */
 	public TreeNode<T> getChildAt(int childIndex);
@@ -23,25 +19,9 @@ public interface TreeNode<T> {
 	 */
 	public T getData();
 
-	/** Returns number of ancestor's generations.
-	 */
-	public int getLevel();
-
 	/** Returns the number of children TreeNodes instance contains.
 	 */
 	public int getNumberOfChildren();
-
-	/** Returns the parent TreeNode of the instance.
-	 */
-	public TreeNode<T> getParent();
-
-	/** Returns the root TreeNode of the instance's tree.
-	 */
-	public TreeNode<T> getRoot();
-
-	/** Find shared ancestor if exists and returns it's instance.
-	 */
-	public TreeNode<T> getSharedAncestor(TreeNode<T> node);
 
 	/** Adds child to the instance at index.
 	 */
@@ -51,17 +31,9 @@ public interface TreeNode<T> {
 	 */
 	public boolean isLeaf();
 
-	/** Returns true if instance has node as one of it's ancestors.
-	 */
-	public boolean hasAncestor(TreeNode<T> node);
-
 	/** Returns true if instance has node as it's child.
 	 */
 	public boolean hasChild(TreeNode<T> node);
-
-	/** Syntactic sugar to node.hasAncestor(this).
-	 */
-	public boolean hasDescendant(TreeNode<T> node);
 
 	/** Removes child at index from the instance.
 	 */
@@ -69,17 +41,5 @@ public interface TreeNode<T> {
 
 	/** Removes child from the instance.
 	 */
-	public boolean remove(TreeNode<T> child);
-
-	/** Removes the instance from it's parent.
-	 */
-	public TreeNode<T> removeParent();
-
-	/** Sets instance's data.
-	 */
-	public TreeNode<T> setData(T data);
-
-	/** Sets the parent of the current instance to newParent.
-	 **/
-	public TreeNode<T> setParent(TreeNode<T> newParent);
+	public TreeNode<T> remove(TreeNode<T> child);
 }
